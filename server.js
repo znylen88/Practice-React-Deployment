70ba30cc31f31c39699a734f47f1ac85ea95e2e0
Zachary Nylen <znylen@aol.com>
2020-02-25 19:01:46 -0800

    Update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb:// practice1:practice1@ds125871.mlab.com:25871/heroku_0xn0jnk7");
 
 // Start the API server
 app.listen(PORT, function() {
